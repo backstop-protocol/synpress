25358992cff176aea2ec3a4572879dfcf998ebd3
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-08-24 10:56:43 +0300

    config: + test retries + uncaught:exceptions

--- support/index.js
+++ support/index.js
@@ -3,6 +3,12 @@ import { configure } from '@testing-library/cypress';
 
 configure({ testIdAttribute: 'data-testid' });
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
 Cypress.on('window:before:load', win => {
   cy.stub(win.console, 'error').callsFake(message => {
     cy.now('task', 'error', message);
